bfac15fe5d53cccb1e11f8ac9a14732a6b404239
SergioGarciaS <sergio.garcia.sti@gmail.com>
2024-02-21 17:34:24 +0100

    titulos

--- tfg.py
+++ tfg.py
@@ -3,10 +3,10 @@ from bs4 import BeautifulSoup
 import json
 
 diccionario = {}
-#url = 'https://dblp.org/pid/r/GregorioRobles.html'
+url = 'https://dblp.org/pid/r/GregorioRobles.html'
 #url = 'https://dblp.org/pid/66/1148.html'
 #url = 'https://dblp.org/pid/117/9939.html'
-url = 'https://dblp.org/db/journals/peerj-cs/peerj-cs7.html#DuenasCGFICG21'
+#url = 'https://dblp.org/db/journals/peerj-cs/peerj-cs7.html#DuenasCGFICG21'
 
 # Solicitud de HTTP
 headers = {
@@ -18,23 +18,47 @@ headers = {
 response = requests.get(url,headers=headers)
 
 # Verificar si la solicitud fue exitosa (código de estado 200)
-if response.status_code == 200:
+def autores():
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+    if response.status_code == 200:
 
-    datos = soup.find_all("span",{"itemprop":"name"})
+        soup = BeautifulSoup(response.text, 'html.parser')
+        datos = soup.find_all("span",{"itemprop":"name"})
 
+        for dato in datos:
+            if dato.get('title') is not None:
+                if dato.get('title') in diccionario:
+                    diccionario[dato.get('title')]+=1
+                else:
+                    diccionario[dato.get('title')]=1
+                #print(dato.get('title'))
+        diccionario_ordenado = dict(sorted(diccionario.items(), key=lambda item: item[1], reverse=True))
+        #print(diccionario)
+        with open('volcado.json', 'w') as jf: 
+            json.dump(diccionario_ordenado, jf, ensure_ascii=False, indent=2)
+    else:
+        print('Error al realizar la solicitud HTTP:', response.status_code)
 
-    for dato in datos:
-        if dato.get('title') is not None:
-            if dato.get('title') in diccionario:
-                diccionario[dato.get('title')]+=1
-            else:
-                diccionario[dato.get('title')]=1
-            #print(dato.get('title'))
-    diccionario_ordenado = dict(sorted(diccionario.items(), key=lambda item: item[1], reverse=True))
-    #print(diccionario)
-    with open('volcado.json', 'w') as jf: 
-        json.dump(diccionario_ordenado, jf, ensure_ascii=False, indent=2)
-else:
-    print('Error al realizar la solicitud HTTP:', response.status_code)
+def titulos():
+    if response.status_code == 200:
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        datos = soup.find_all("span",{"class":"title"},)
+        
+        for dato in datos:
+            titulos =dato.get_text()
+            print(titulos)
+            if dato.get('dato') is not None:
+                if dato.get('dato') in diccionario:
+                    diccionario[dato.get('dato')]+=1
+                else:
+                    diccionario[dato.get('name')]=1
+                #print(dato.get('title'))
+        diccionario_ordenado = dict(sorted(diccionario.items(), key=lambda item: item[1], reverse=True))
+        #print(diccionario)
+        with open('volcado.json', 'w') as jf: 
+            json.dump(diccionario_ordenado, jf, ensure_ascii=False, indent=2)
+    else:
+        print('Error al realizar la solicitud HTTP:', response.status_code)    
+        
+titulos()
